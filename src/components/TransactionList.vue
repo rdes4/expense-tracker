5e6090925f16b143fd5ba83e89d452ae0d7337a0
AthiyyahRachmazaine9999 <rifqides21@gmail.com>
2023-11-15 11:04:41 +0700

    Commit #1

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <!-- <li class="minus">
+            Cash <span>-Rp 4,000</span><button class="delete-btn">X</button>
+        </li>
+        <li class="plus">
+            Paycheck <span>+Rp 7,000</span><button class="delete-btn">X</button>
+        </li> -->
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>Rp {{ transaction.amount }}</span><button @click="deleteTransaction(transaction.id)" class="delete-btn">X</button>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+    import { defineProps } from "vue";
+
+    const emit = defineEmits(['transactionDeleted'])
+
+    const props = defineProps({
+        transactions:{
+            type: Array,
+            requred: true
+        }
+    })
+
+    const deleteTransaction = (id) => {
+        emit('transactionDeleted', id);
+    }
+</script>
